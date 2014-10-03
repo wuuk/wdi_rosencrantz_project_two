9b1eb0c3a32b350f2077386a656c3cdf1bf0a4d3
wuuk <alexfong@hotmail.com>
2014-10-03 11:56:22 -0400

    add Neel's dark side to the enemies list, because we do not want to be on Neel's dark side

--- seeds.rb
+++ seeds.rb
@@ -8,7 +8,9 @@ Contact.delete_all
 
 friends = Category.create(name: "friends")
 frenemies = Category.create(name: "frenemies")
+enemies = Category.create(name: "enemies")
 
 Contact.create(name: "Sean", category_id: friends.id)
 Contact.create(name: "Jeff", category_id: friends.id)
 Contact.create(name: "Neel", category_id: frenemies.id)
+Contact.create(name: "DarthNeel", catagory_id: enemies.id)
